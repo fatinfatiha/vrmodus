33e949da1f1b5f1f4df44cd57ba4415876f0d70e
Fitim <fitafejzullahu24@gmail.com>
2016-05-25 19:50:56 +0800

    homepage done

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,9 +1,47 @@
 @extends('masterLayout.master')
 @section('content')
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<section class="section-home slide-home text-home">
+         <div class="container">
+                <div class="row">
+            </div>
+          </div>
+</section>
+<div class="container">
+   <div class="row img-logo">
+         <img src="img/vrLogo.png" alt="logo" class="img-responsive center-block"/>
+   </div>
+   <div class="row">
+    <div class=" col-md-6 col-md-offset-3">
+      <div class="home-text">
+       <h1>Expanding Your Vision</h1>
+     </div>
+   </div>
+  </div>
+   <div class="row">
+    <div class=" col-md-6 col-md-offset-3  text-center">
+         <h3>You, as an individual or organization can use our brand to certify the knowledge, skills and talents of others. Doing so will enchance your credibility. </h3>
+ </div>
+</div>
+<div class="row img-icon">
+      <img src="img/home/VRmodusPack-04.png" alt="icons" class="img-responsive center-block"/>
+</div>
+<div class="row img-logo">
+      <img src="img/home/VRmodusPack-06.png" alt="icons" class="img-responsive center-block"/>
+      <div class=" col-md-6 col-md-offset-3  text-center">
+           <h3>You, as an individual or organization can use our brand to certify the knowledge, skills and talents of others. Doing so will enchance your credibility. </h3>
+   </div>
+</div>
+<div class="row img-icon">
+      <img src="img/home/VRmodusPack-04.png" alt="icons" class="img-responsive center-block"/>
+</div>
+<div class="row">
+      <div class=" col-md-6 home-text">
+           <h2>You, as an individual or organization can use our brand to certify the knowledge, skills and talents of others. Doing so will enchance your credibility. </h2>
+   </div>
+   <div class=" col-md-6" style="margin-bottom:50px;">
+           <img src="img/home/VRmodusPack-05.png" alt="icons" class="img-responsive center-block"/>
+   </div>
+
+</div>
+ </div>
 @stop
